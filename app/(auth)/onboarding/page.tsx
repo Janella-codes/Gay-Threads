272583a5e7c95fd5c6766c864555ed49e07e639e
Janella-codes <118861074+Janella-codes@users.noreply.github.com>
2024-04-03 08:38:33 -0700

    Add auth

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,11 +1,18 @@
 import AccountProfile from "@/components/forms/AccountProfile"
+import { fetchUser } from "@/lib/actions/user.actions";
 import { UserButton, currentUser } from "@clerk/nextjs"
+import { redirect } from "next/navigation";
 
 
 async function Page() {
     const user = await currentUser();
+    if (!user) return null; // to avoid typescript warnings
 
-    const userInfo = {};
+    const userInfo = await fetchUser(user.id);
+    if (userInfo?.onboarded) redirect("/");
+
+
+
 
     const userData = {
         id: user?.id,
@@ -15,6 +22,9 @@ async function Page() {
         bio: userInfo?.bio || "",
         image: userInfo?.image || user?.imageUrl,
     }
+
+
+
 
     return (
         <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
