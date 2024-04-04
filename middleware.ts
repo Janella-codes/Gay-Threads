fcadba02940439e72839dd50e5e6496e873e9e80
Janella-codes <118861074+Janella-codes@users.noreply.github.com>
2024-04-04 09:27:53 -0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,10 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ['/profile/null','/','/api/webhook/clerk'],
+  publicRoutes: ['/','/profile/null','/api/webhook/clerk'],
+
+ // An array of routes to be ignored by the authentication middleware.
+  ignoredRoutes: ["/api/webhook/clerk"],
 
 });
  
