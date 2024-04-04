0f43f4fbc7063f80a4fa61df0ddd95a76e70084a
Janella-codes <118861074+Janella-codes@users.noreply.github.com>
2024-04-04 09:06:21 -0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,16 @@
-import { authMiddleware } from "@clerk/nextjs";
- 
-// See https://clerk.com/docs/references/nextjs/auth-middleware
-// for more information about configuring your Middleware
- 
-export default authMiddleware({
-    publicRoutes: ['/','/sign-in','/api/webhook/clerk'],
+// Resource: https://clerk.com/docs/nextjs/middleware#auth-middleware
+// Copy the middleware code as it is from the above resource
 
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({
+  // An array of public routes that don't require authentication.
+  publicRoutes: ["/api/webhook/clerk"],
+
+  // An array of routes to be ignored by the authentication middleware.
+  ignoredRoutes: ["/api/webhook/clerk"],
 });
 
 export const config = {
-  matcher: [
-    // Exclude files with a "." followed by an extension, which are typically static files.
-    // Exclude files in the _next directory, which are Next.js internals.
- 
-    "/((?!.+\\.[\\w]+$|_next).*)",
-    // Re-include any files in the api or trpc folders that might have an extension
-    "/(api|trpc)(.*)"
-  ]
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
